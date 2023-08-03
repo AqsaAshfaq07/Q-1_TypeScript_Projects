6a7aaa66a52b966fd5223190b20f0ba68f7410bd
Aqsa Ashfaq <aqsarao077@gmail.com>
2023-08-03 11:55:38 +0500

    class_1

---
+++ assignment_1.js
@@ -0,0 +1,29 @@
+"use strict";
+// - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Fam, I am very excited to share that I've started a new BootCamp of Web3, Metaverse and AI at PIAIC. ");
+// - Store your name in a variable and print it.
+var my_name = "AQSA ASHFAQ";
+console.log(my_name);
+//  - Store 10 numbers in different variables.
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+var num6 = 6;
+var num7 = 7;
+var num8 = 8;
+var num9 = 9;
+var num10 = 10;
+// - Add all of them and print the SUM
+var sum = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log(sum);
+// - Print the Difference (subtraction).
+var difference = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log(difference);
+// - Print the result after multiplying all.
+var multiplication = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log(multiplication);
+// - Take two numbers and print the division result.
+var division = num1 / num2;
+console.log(division);
---
+++ assignment_1.ts
@@ -0,0 +1,36 @@
+"use strict"
+
+// - Print a message like "Hey Abu Hurairah, I have started learning TYPESCRIPT."
+console.log("Hey Fam, I am very excited to share that I've started a new BootCamp of Web3, Metaverse and AI at PIAIC. ");
+
+// - Store your name in a variable and print it.
+var my_name:String = "AQSA ASHFAQ";
+console.log(my_name);
+
+//  - Store 10 numbers in different variables.
+var num1:number = 1;
+var num2:number = 2;
+var num3:number = 3;
+var num4:number = 4;
+var num5:number = 5;
+var num6:number = 6;
+var num7:number = 7;
+var num8:number = 8;
+var num9:number = 9;
+var num10:number = 10;
+
+// - Add all of them and print the SUM
+var sum:number = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+console.log(sum);
+
+// - Print the Difference (subtraction).
+var difference:number = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10;
+console.log(difference);
+
+// - Print the result after multiplying all.
+var multiplication:number = num1 * num2 * num3 * num4 * num5 * num6 * num7 * num8 * num9 * num10;
+console.log(multiplication);
+
+// - Take two numbers and print the division result.
+var division:number = num1 / num2;
+console.log(division);
